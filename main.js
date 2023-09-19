ae6d1743a13d897fe952c5cf7cadcb61d24adb11
OmololaBecca <folarebecca03@gmail.com>
2023-09-19 17:09:47 +0100

    second commit for weather-forecast project

--- main.js
+++ main.js
@@ -1,8 +1,100 @@
 const api ='f8f1bd81386b538512bbd69470d8ffa9';
+const searchIcon =document.getElementById('search-icon')
+const city= document.getElementById('location')
+const weatherDisplay=document.querySelector('.weather-display-div')
+const sunrise=document.querySelector('.sunrise')
+const weatherCondition=document.querySelector('.weather-condition')
 
-const fetchApi = async () =>{
-    let data = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=London&appid=${api}`);
-   let fetchedData = await data.json()
-   console.log(fetchedData)
-}
-fetchApi()
+
+
+
+
+searchIcon.addEventListener('click' ,() =>{
+    const fetchApi = async () =>{
+        let data = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city.value}&appid=${api}`);
+       let fetchedData = await data.json()
+       console.log(fetchedData)
+    //    console.log(fetchedData.weather[0].description)
+    //    console.log(fetchedData.name)
+    //    console.log(fetchedData.sys.country)
+    
+    //    let searchLocation = fetchedData.name;
+    //    let searchCountry = fetchedData.sys.country;
+    
+    //    console.log(`${searchLocation}, ${searchCountry}`) 
+    let displayCard =document.createElement("div")
+    displayCard.className ="displayed-card" 
+
+    let searchedCityWeather=document.createElement("p")
+    searchedCityWeather.className="searched-city-weather"
+
+    let degreeAndP =document.createElement("div")
+    degreeAndP.className ="degree-div"
+    degreeAndP.appendChild(document.createTextNode("18"))
+
+    // let searchedDescp =document.createElement("p")
+    // searchedDescp.className ="searched-descp"
+
+    let degree = document.createElement("sup")
+    degree.className="degree"
+    degree.appendChild(document.createTextNode("0"))
+    console.log(degree)
+
+    degreeAndP.appendChild(document.createTextNode("C"))
+
+    let cityName = document.createElement("p")
+    cityName.className="city-name"
+
+
+
+    //  degreeAndP.appendChild(searchedDescp)
+     degreeAndP.appendChild(degree)
+     displayCard.appendChild(searchedCityWeather)
+     displayCard.appendChild(degreeAndP)
+     displayCard.appendChild(cityName)
+     weatherDisplay.appendChild(displayCard)
+
+
+
+    
+     let receivedDescp = fetchedData.weather[0].main
+     let receivedData = Math.floor(fetchedData.main.temp)
+     let cityDataName =fetchedData.name
+
+     
+     searchedCityWeather.innerText= receivedDescp;
+      degreeAndP.innerText= receivedData;
+     cityName.innerText = cityDataName;
+
+
+     //the changing of the images
+ let randomImages=searchedCityWeather.innerText
+     if(randomImages=="Clouds"){
+       document.body.style.backgroundImage= "url('./Assets/jeremy-thomas-jh2KTqHLMjE-unsplash.jpg')"
+       document.body.style.backgroundSize = "cover"
+       document.body.style.backgroundRepeat = "no repeat"
+       document.body.style.backgroundPosition = "center"
+     }else if(randomImages=="Rain"){
+        document.body.style.backgroundImage = "url('/Assets/raimond-klavins-rxm_S9XmK-Y-unsplash.jpg')"
+        searchedCityWeather.style.color="black"
+        cityName.style.color="black"
+         degreeAndP.style.color="black"
+     }else if(randomImages=="wind"){
+       document.body.style.backgroundImage="url"('./Assets/khamkeo-vilaysing-WtwSsqwYlA0-unsplash.jpg')
+     }else if(randomImages=="Sunny"){
+      document.body.style.backgroundImage ="url('./Assets/aaron-burden-BTubi6qaY6Q-unsplash.jpg')"
+     }else{
+         document.body.style.backgroundImage ="url('./Assets/tom-barrett-hgGplX3PFBg-unsplash.jpg')"
+     }
+     
+    }
+    fetchApi()
+     
+    
+})
+
+
+
+
+
+
